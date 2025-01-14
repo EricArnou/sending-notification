497fa8ba6494c3f2d20b14e99e3653aee7e7a35f
Eric Arnou Santos <ericarnou68@gmail.com>
2025-01-14 11:22:17 -0300

    feat: create Repository and Docker compose file with postgreSQL databse closes #2

--- src/main/java/com/gmail/ericarnou68/sending/notification/entities/Notification.java
+++ src/main/java/com/gmail/ericarnou68/sending/notification/entities/Notification.java
@@ -2,15 +2,19 @@ package com.gmail.ericarnou68.sending.notification.entities;
 
 import jakarta.persistence.*;
 
+import java.util.UUID;
+
 @Entity
 @Table(name = "tb_notifications")
 public class Notification {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private Long id;
+    private UUID id;
 
     private String recipient;
+
+    private String message;
 
     @Enumerated(EnumType.STRING)
     private Chanel chanel;
