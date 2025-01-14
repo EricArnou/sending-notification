6c90171f56615c6f45f756fe301c3de4c103af89
Eric Arnou Santos <ericarnou68@gmail.com>
2025-01-14 00:32:43 -0300

    initial environment

---
+++ src/main/java/com/gmail/ericarnou68/sending/notification/Application.java
@@ -0,0 +1,13 @@
+package com.gmail.ericarnou68.sending.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/ericarnou68/sending/notification/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.ericarnou68.sending.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
