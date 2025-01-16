588805e9b9f89424b3773d08f7df1d9e79995745
Eric Arnou Santos <ericarnou68@gmail.com>
2025-01-16 18:21:22 -0300

    feat: created schedule notification dto closes #4

--- src/main/java/com/gmail/ericarnou68/sending/notification/entities/dto/ScheduleNotificationDto.java
+++ src/main/java/com/gmail/ericarnou68/sending/notification/entities/dto/ScheduleNotificationDto.java
@@ -1,4 +1,9 @@
 package com.gmail.ericarnou68.sending.notification.entities.dto;
 
-public record ScheduleNotificationDto() {
+import com.gmail.ericarnou68.sending.notification.entities.Chanel;
+import com.gmail.ericarnou68.sending.notification.entities.Status;
+
+import java.time.LocalDateTime;
+
+public record ScheduleNotificationDto(String recipient, String message, LocalDateTime scheduling, Chanel chanel, Status status) {
 }
